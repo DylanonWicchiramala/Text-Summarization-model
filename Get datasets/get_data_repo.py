338a7e644972af134300b0003067abebdc5a54c3
Dylanon Wicchiramala <dylanonwic@gmail.com>
2023-09-27 10:23:08 +0700

    try catch on git clone

--- Get datasets/get_data_repo.py
+++ Get datasets/get_data_repo.py
@@ -9,7 +9,7 @@ import re
 import git
 
 datasets_source_path = os.path.abspath("Get datasets/datasets_source.csv")
-test_run = True
+test_run = False
 
 sources = pd.read_csv(datasets_source_path, index_col="Unique").dropna(subset="Link_to_download")
 
@@ -35,7 +35,11 @@ print()
 #clone the datasets repository.
 for source in cloneable_list:
     print(f"Cloning {source.Name} to {source.Local_repo_dir}")
-    if not test_run: git.Repo.clone_from(source.Link_to_download, to_path=source.Local_repo_dir)
+    if not test_run: 
+        try:
+            git.Repo.clone_from(source.Link_to_download, to_path=source.Local_repo_dir)
+        except Exception as e:
+            print(f"An error occurred while cloning {source.Link_to_download}: {e}")    
     
 print(f"cloned all datasets repositories")
 
