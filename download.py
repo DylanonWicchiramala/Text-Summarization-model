8550742037e5a03a7b7d4e300fd400213735ab7a
Dylanon Wicchiramala <dylanonwic@gmail.com>
2023-10-03 16:33:26 +0700

    .

---
+++ download.py
@@ -0,0 +1,6 @@
+import wandb
+import credentials as cre
+wandb.login(key=cre.wandb_key)
+run = wandb.init()
+artifact = run.use_artifact('dylanon/text-summarization-model/datasets:v0', type='dataset')
+artifact_dir = artifact.download(root="datasets")
